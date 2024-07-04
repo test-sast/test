3d235207a38035dfd739205c26a5f7e03566ae85
Anton Abrosymov <62562060+aabrosymov@users.noreply.github.com>
2024-07-04 12:21:45 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+proxy = {
+  'https': 'http://127.0.0.1:8090',
+}
+auth_dict = {'username': 'admin', 'password': 'admin123'}
+login = requests.post(target_url + '/login',
+                      proxies=proxies, json=auth_dict, verify=False)
+if login.status_code == 200:  # if login is successful
+    auth_token = login.headers['Authorization']
+    auth_header = {"Authorization": auth_token}
+    
